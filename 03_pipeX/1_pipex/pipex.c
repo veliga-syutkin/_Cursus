ae3a3d5a4a8f10d1c41cea6e682ea4a576690eed
Veliga Syutkin <vsyutkin@k1r3p6.42mulhouse.fr>
2024-01-02 12:51:57 +0100

    PipeX v0.1.0

--- 03_pipeX/1_pipex/pipex.c
+++ 03_pipeX/1_pipex/pipex.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsyutkin <vsyutkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/02 11:55:40 by vsyutkin          #+#    #+#             */
-/*   Updated: 2024/01/02 12:14:04 by vsyutkin         ###   ########.fr       */
+/*   Created: 2024/01/02 11:59:07 by vsyutkin          #+#    #+#             */
+/*   Updated: 2024/01/02 12:16:03 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-//Making sure every function's biblio is included
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <sys/wait.h>
-# include <stdlib.h>
-
-//=============================================================================
-
-
-
-
-#endif 
+int	main(int argc, char **argv)
+{
+	
+}
