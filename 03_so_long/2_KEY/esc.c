f261444f61d7b900380fff63fa4deb6a4b64e9ae
Veliga Syutkin <vsyutkin@k2r2p10.42mulhouse.fr>
2024-05-20 23:32:30 +0200

    v7 added valid maps for tests

--- 03_so_long/2_KEY/esc.c
+++ 03_so_long/2_KEY/esc.c
@@ -6,18 +6,23 @@
 /*   By: vsyutkin <vsyutkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:53:36 by vsyutkin          #+#    #+#             */
-/*   Updated: 2024/05/20 22:54:13 by vsyutkin         ###   ########.fr       */
+/*   Updated: 2024/05/20 23:31:39 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
+static void	close_window(void *param)
+{
+	ft_printf("Key pressed: ESC\n");
+	mlx_close_window(param);
+	ft_printf("Initiated window closing\n");
+}
+
 void	key_esc(mlx_key_data_t keydata, void *param)
 {
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_RELEASE)
 	{
-		ft_printf("Key pressed: ESC\n");
-		mlx_close_window(param);
-		ft_printf("Initiated window closing\n");
+		close_window(param);
 	}
 }
