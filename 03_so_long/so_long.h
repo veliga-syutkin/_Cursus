4a43f074ade3dd7a689226f37375994e89780f62
Veliga Syutkin <vsyutkin@k1r1p2.42mulhouse.fr>
2024-05-26 13:10:12 +0200

    v20 added another unvalid map

--- 03_so_long/so_long.h
+++ 03_so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: vsyutkin <vsyutkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 21:36:41 by vsyutkin          #+#    #+#             */
-/*   Updated: 2024/05/25 23:03:58 by vsyutkin         ###   ########.fr       */
+/*   Updated: 2024/05/26 13:06:01 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 # define WIDTH 1024
 # define HEIGHT 1024
+
 # define WALL '1'
 # define EXIT 'E'
 # define FLOOR '0'
@@ -36,7 +37,7 @@
 # define ERR_MAP_PATH "Error\nMap path is wrong\n"
 # define ERR_MAP_OPEN "Error\nCouldn't open map file\n"
 # define ERR_MAP_EXTENSION "Error\nMap file extension is not .ber\n"
-# define ERR_MAP_CONTENT "Error\nMap content is invalid\n"
+# define ERR_MAP_CONTENT "Error\nMap is corrupted\n"
 # define ERR_MAP_DEAD_END "Error\nMap is impossible to solve\n"
 
 ////////////////////////////////////////////////////////////////////////////////
